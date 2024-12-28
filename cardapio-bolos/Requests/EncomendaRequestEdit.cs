8f0926952946332adec3a2dc5844e7ffdff4bdcf
StephanieGdSantos <gomess214@gmail.com>
2024-12-27 23:39:35 -0300

    adiciona encomendarequestedit

---
+++ cardapio-bolos/Requests/EncomendaRequestEdit.cs
@@ -0,0 +1,7 @@
+﻿using CardapioBolos.Model;
+using System.Collections.Generic;
+
+namespace CardapioBolos.Requests
+{
+    public record EncomendaRequestEdit(string TelefoneCliente, List<Bolo> Bolos, double ValorFinal, bool Finalizado, DateTime DataDaEntrega) { }
+}
