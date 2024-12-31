e234580a458913eedf6955009126aaa40c831650
StephanieGdSantos <gomess214@gmail.com>
2024-12-30 21:05:25 -0300

    adiciona serviços para o admin

---
+++ cardapio-bolos/Services/AdministradorServices.cs
@@ -0,0 +1,45 @@
+﻿using CardapioBolos.Banco;
+using CardapioBolos.Model;
+using CardapioBolos.Utils;
+using System.Security.Claims;
+
+namespace CardapioBolos.Services
+{
+    public class AdministradorServices
+    {
+        private readonly DAL<Administrador> _administradorDAL;
+        private readonly CardapioBolosContext _context;
+
+        public AdministradorServices(CardapioBolosContext context)
+        {
+            _administradorDAL = new DAL<Administrador>(context);
+            _context = context;
+        }
+        public bool Autenticar(string email, string senha)
+        {
+            var administrador = _administradorDAL.Buscar(admin => admin.Email.Equals(email));
+            if (administrador == null)
+                return false;
+
+            return VerificadorDeSenha.VerificarSenha(senha, administrador.Senha);
+        }
+
+        public bool VerificarSeEmailJaEhUsado(string email)
+        {
+            var administrador = _administradorDAL.Buscar(admin => admin.Email.Equals(email));
+
+            if (administrador == null)
+                return false;
+            return true;
+        }
+
+        public bool ValidaSeEhAdministrador(ClaimsPrincipal usuario)
+        {
+            var regra = usuario.FindFirst(ClaimTypes.Role);
+            if (regra == null || regra.Value != "admin")
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ cardapio-bolos/Utils/VerificadorDeSenha.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CardapioBolos.Utils
+{
+    public class VerificadorDeSenha
+    {
+        public static string GerarHashSenha(string senha)
+        {
+            using var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(senha);
+            var hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool VerificarSenha(string senhaInserida, string hashArmazenado)
+        {
+            var hashDaSenhaInserida = GerarHashSenha(senhaInserida);
+            return hashDaSenhaInserida == hashArmazenado;
+        }
+    }
+}
