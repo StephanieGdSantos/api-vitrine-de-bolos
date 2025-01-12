d64f244a9adee29dcfe4be1120293f38d618b85d
StephanieGdSantos <gomess214@gmail.com>
2025-01-12 18:09:30 -0300

    abre documentação do swagger sem autenticação

--- cardapio-bolos/Autenticacao/ApiKeyMiddleware.cs
+++ cardapio-bolos/Autenticacao/ApiKeyMiddleware.cs
@@ -14,6 +14,12 @@ public class ApiKeyMiddleware
 
     public async Task InvokeAsync(HttpContext context, IConfiguration configuration)
     {
+        if (context.Request.Path.StartsWithSegments("/swagger"))
+        {
+            await _next(context);
+            return;
+        }
+
         if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
         {
             context.Response.StatusCode = 401;
