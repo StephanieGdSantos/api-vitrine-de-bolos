b963c20f139d7a170114b9951c76e6bb7ad754b9
StephanieGdSantos <gomess214@gmail.com>
2025-01-12 23:37:06 -0300

    Revert "exclui cardapio-bolos/Banco/CardapioBolosContext.cs"
    This reverts commit 5b382c9b82b5237ea5c115b772dd53b9d4bbafe5.

---
+++ cardapio-bolos/Banco/CardapioBolosContext.cs
@@ -0,0 +1,39 @@
+﻿using CardapioBolos.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardapioBolos.Banco
+{
+    public class CardapioBolosContext: DbContext
+    {
+        public DbSet<Bolo> Bolos { get; set; }
+        public DbSet<Encomenda> Encomendas { get; set; }
+        public DbSet<Administrador> Administrador { get; set; }
+
+        private string connectionString = "Server=host.docker.internal,1433;Database=CardapioDeBolos;Trusted_Connection=false;Encrypt=false;User Id=sa;Password=BDVitrine#10;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Encomenda>()
+                .HasMany(e => e.Bolos)
+                .WithMany(b => b.Encomendas)
+                .UsingEntity<Dictionary<string, object>>(
+                    "BoloEncomenda",
+                    j => j.HasOne<Bolo>().WithMany().HasForeignKey("BoloId"),
+                    j => j.HasOne<Encomenda>().WithMany().HasForeignKey("EncomendaId")
+                );
+
+            modelBuilder.Entity<Bolo>()
+                .HasOne(b => b.Administrador)
+                .WithMany(a => a.Bolos)
+                .HasForeignKey(b => b.AdministradorId);
+        }
+    }
+}
