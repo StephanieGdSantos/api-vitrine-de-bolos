b03c8a1daa99291fccc5314e0eedcd483da79e9f
StephanieGdSantos <gomess214@gmail.com>
2025-01-31 22:06:59 -0300

    renomeia projeto

--- CardapioBolos/obj/Debug/net8.0/cardapio-bolos.AssemblyInfo.cs
+++ CardapioBolos/obj/Debug/net8.0/cardapio-bolos.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("cardapio-bolos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CardapioBolos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("cardapio-bolos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("cardapio-bolos")]
+[assembly: System.Reflection.AssemblyProductAttribute("CardapioBolos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CardapioBolos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# api-cardapio-bolos
+# api-CardapioBolos
